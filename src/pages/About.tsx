36bba9f593d55b1a4ad2365f3f6aca0f1fdbadd2
Abinesh <abineshkalyan2002@gmail.com>
2026-01-05 21:03:52 +0530

    Change profile image from JPG to PNG

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -44,7 +44,7 @@ const About = () => {
 				<ScrollAnimation>
 					<div className="aspect-square overflow-hidden rounded-2xl">
 						<img
-							src="/profile/profile.jpg"
+							src="/profile/profile.png"
 							alt="Abinesh Kalyanasundaram"
 							className="w-full h-full object-cover hover:scale-105 transition-transform duration-500"
 						/>
